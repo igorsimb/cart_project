a89b9f95d0caf26bff6f9d9567890cdadae01e75
igorsimb <gorets@gmx.com>
2021-06-29 11:08:52 +0300

    Price in Your Cart now dynamically updates.

--- cart/views.py
+++ cart/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, redirect
+from django.db.models import Sum, Min, Max, Avg
 
 from .models import Topping
 
 def index(request):
     topping_list = Topping.objects.all()
-    context = {'topping_list': topping_list}
+    # show price Sum of toppings that were added to Your Cart
+    topping_price = Topping.objects.filter(is_added=True).aggregate(total_price=Sum('price'))
+    context = {'topping_list': topping_list, 'topping_price': topping_price}
     return render(request, 'cart/index.html', context)
 
 
 def toppingAdd(request, topping_id):
     topping = Topping.objects.get(pk=topping_id)
+    # topping_price = Topping.objects.get(pk=topping_id).aggregate(total_price=Sum('price'))
+
     topping.is_added = True
 
-    #TODO: create total for all topping prices and make it show in Template
+
     print(f'TOPPING PRICE = {topping.price}')
 
     topping.save()
+    # topping_price.save()
 
     return redirect('index')
 
